c11399b83310d6d12399b100a2b1eb28ef199187
AUDNQ <audnqq@gmail.com>
2026-09-05 16:52:38 +1000

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -11,21 +11,17 @@ menu = ReplyKeyboardMarkup(
 
 free_ai = InlineKeyboardMarkup(
     inline_keyboard=[
-            [
-                InlineKeyboardButton(text="Gemini 3.7 Flash", callback_data="Ai_Gemini 3.7 Flash"),
-                InlineKeyboardButton(text="Qwen3.7 Plus", callback_data="Ai_Qwen3.7 Plus"),
-                InlineKeyboardButton(text="Claude 3 Haiku", callback_data="Ai_Claude 3 Haiku")
-            ]
+            [InlineKeyboardButton(text="Gemini 3.7 Flash", callback_data="Ai_Gemini 3.7 Flash")],
+            [InlineKeyboardButton(text="Qwen3.7 Plus", callback_data="Ai_Qwen3.7 Plus")],
+            [InlineKeyboardButton(text="Claude 3 Haiku", callback_data="Ai_Claude 3 Haiku")]
     ]
 )
 
 
 pro_ai = InlineKeyboardMarkup(
     inline_keyboard=[
-            [
-                InlineKeyboardButton(text="Grok 4.6", callback_data="Ai_Grok 4.6"),
-                InlineKeyboardButton(text="GPT 5.4 mini", callback_data="Ai_GPT 5.4 mini"),
-                InlineKeyboardButton(text="Claude Haiku 4.5", callback_data="Ai_Claude Haiku 4.5") 
-            ],
+                [InlineKeyboardButton(text="Grok 4.6", callback_data="Ai_Grok 4.6")],
+                [InlineKeyboardButton(text="GPT 5.4 mini", callback_data="Ai_GPT 5.4 mini")],
+                [InlineKeyboardButton(text="Claude Haiku 4.5", callback_data="Ai_Claude Haiku 4.5")] 
     ]
 )
