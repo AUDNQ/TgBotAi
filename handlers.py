e499ef97c51296c28121eee71458f52f8290147e
AUDNQ <audnqq@gmail.com>
2026-09-05 20:35:49 +1000

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -23,7 +23,7 @@ async def cmd_start(message: Message):
         return
     answer = await user_register(user_id)
     if answer:
-        await message.reply("Вы зарегистрированны напишите команду !help.", reply_markup = kb.menu)
+        await message.reply("Вы зарегистрированны напишите команду !help.")
     else:
         await message.reply("Вы уже зарегистрированны.")
 
