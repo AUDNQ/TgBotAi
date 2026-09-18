f436c974030af3d960f78c9971397416dd309736
AUDNQ <audnqq@gmail.com>
2026-09-18 20:33:14 +1000

    Add files via upload

---
+++ handlers/callback_handlers.py
@@ -0,0 +1,85 @@
+from aiogram.types import CallbackQuery
+from aiogram import F, Router
+from core.db import view_user,  add_data
+from services.chat_engine import get_simple_user_id
+from core.loader import app, client
+import logging
+from services.chat_engine import get_channel_message, process_channel_ai
+
+logging.basicConfig(level=logging.INFO)
+
+router = Router()
+
+@router.callback_query(F.data.startswith("Ai_"))
+async def chek_ai(callback: CallbackQuery):
+    user_ai = callback.data.split("_")[1]
+    try:
+        user_id = get_simple_user_id(callback=callback)
+        answer = await add_data(user_id, user_ai=user_ai)
+        if answer:
+            await callback.answer(f"Модель изменина")
+            await callback.message.edit_text(f"ИИ модель изменена.", reply_markup=None)
+        else:
+            await callback.message.reply("Ошибка попробуйте еще раз.")
+    except Exception:
+            logging.exception("Ошибка при выборе AI")
+
+
+@router.callback_query(F.data.startswith("deletechannel:"))
+async def delete_channel(callback: CallbackQuery):
+    user_id = get_simple_user_id(callback=callback)
+    if not user_id:
+        return
+
+    user_data = await view_user(user_id)
+    updated_channels = []
+    dictionary_channel_id = user_data.get("dictionary_channel_id") or []
+    channel_id_to_delete = callback.data.split(":")[1]
+
+    for item in dictionary_channel_id:
+        if str(list(item.values())[0]) != str(channel_id_to_delete):
+            updated_channels.append(item)
+  
+    if len(updated_channels) == len(dictionary_channel_id):
+        await callback.answer("Канал не найден в списке.")
+        return
+    
+    await add_data(user_id, dictionary_channel_id=updated_channels)
+    await callback.message.edit_text("Канал удален из списка.", reply_markup=None)
+
+
+@router.callback_query(F.data.startswith("channeldays:"))
+async def view_channel_days(callback: CallbackQuery):
+    user_id = get_simple_user_id(callback=callback)
+    if not user_id:
+        return
+    
+    user_data = await view_user(user_id)
+    dictionary_channel_id = user_data.get("dictionary_channel_id") or []
+    channel_id_to_view = callback.data.split(":")[1]
+    
+    channel_info = next((item for item in dictionary_channel_id if str(list(item.values())[0]) == str(channel_id_to_view)), None)
+    
+    if not channel_info:
+        await callback.message.edit_text("Канал не найден в списке.")
+        return
+
+    channel_title = list(channel_info.keys())[0]
+    channel_id = int(list(channel_info.values())[0])
+
+    text = await get_channel_message(channel_id, app)
+    if text is None:
+        await callback.message.edit_text(f"Сообщений небыло")
+        return
+    if text == "Error":
+        await callback.message.edit_text(f"Ошибка при получении сообщений из канала {channel_title}.")
+        return
+    try:
+        ai_answer = await process_channel_ai(user_id, client, text)
+        await callback.message.edit_text(
+            ai_answer,
+            parse_mode="Markdown"
+            )
+    except Exception:
+        await callback.message.edit_text("Ошибка ии =(")
+        logging.exception("Ошибка при обработке сообщения")
---
+++ handlers/fsm_handlers.py
@@ -0,0 +1,76 @@
+from aiogram.types import Message
+from aiogram import Router
+from aiogram.fsm.context import FSMContext
+from core.db import add_privilege, is_user_registered, view_user, add_data
+from core.loader import bot
+from core.data import AddProState, AddTitleChanel
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+router = Router()
+
+@router.message(AddProState.user_id_privilege)
+async def process_add_pro(message: Message, state: FSMContext):
+    try:
+        text = (message.text)
+        text = text.replace("!id", "").strip()
+        user_id_privilege = int(text)
+    except Exception:
+        logging.exception("Ошибка при вводе ID пользователя")
+        await message.reply("Ошибка! Попробуйте еще раз. Введите корректный ID пользователя.")
+        await state.clear()
+        return
+    answer = await is_user_registered(user_id_privilege)
+    if answer:
+        try:
+            chat = await bot.get_chat(user_id_privilege)
+            username = chat.username
+        except Exception:
+            logging.exception("Ошибка при получении юзернейма пользователя")
+            await message.reply(f"Не удалось получить юзернейм пользователя с ID {user_id_privilege}. Пожалуйста, убедитесь, что ID правильный.")
+            await state.clear()
+            return 
+        await message.reply(f"Вы хотите дать подписку pro пользователю с ID {user_id_privilege} и юзернеймом @{username}. Подтвердите '!да' или '!нет'.")
+        await state.update_data(user_id_privilege=user_id_privilege)
+        await state.set_state(AddProState.confirm_privilege)
+    else:
+        await message.reply(f"Пользователь с ID {user_id_privilege} не зарегистрирован. Попросите его зарегистрироваться через !start.")
+        await state.clear()
+        return
+
+
+@router.message(AddProState.confirm_privilege)
+async def confirm_add_pro(message: Message, state: FSMContext):
+    data = await state.get_data()
+    user_id_privilege = data.get("user_id_privilege")
+    if message.text.lower() == "!да":
+        answer = await add_privilege(user_id_privilege, "pro")
+        if answer:
+            await message.reply(f"Пользователю с ID {user_id_privilege} успешно предоставлена подписка pro.")
+            await state.clear()
+        else:
+            await message.reply(f"Не удалось предоставить подписку pro пользователю с ID {user_id_privilege}.")
+            await state.clear()
+    elif message.text.lower() == "!нет":
+        await message.reply(f"Вы отказались предоставить подписку pro пользователю с ID {user_id_privilege}.")
+        await state.clear()
+
+
+@router.message(AddTitleChanel.user_title_channel)
+async def update_title(message: Message, state: FSMContext):
+    data = await state.get_data()
+    user_id = int(data.get("user_id"))
+    user_id_channel = data.get("user_id_channel")
+
+    title_channel = message.text
+
+    user_data = await view_user(user_id)
+    dictionary_channel_id = user_data.get("dictionary_channel_id") or []
+    dictionary_channel_id.append({title_channel: user_id_channel})
+    await add_data(user_id, dictionary_channel_id=dictionary_channel_id)
+
+    await message.answer(
+        f"ID выбранного канала: `{user_id_channel}`, канал добавлен в список.",
+        parse_mode="Markdown"
+    )
